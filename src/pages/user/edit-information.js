09b4ed1b62026e9b889d2a905dda8e338c14b91e
zia ur rehman khan <ziaurrehmanwarsi5@gmail.com>
2023-04-28 23:16:41 +0500

    component name change

--- src/pages/user/edit-information.js
+++ src/pages/user/edit-information.js
@@ -1,16 +1,16 @@
-// Components 
-import UserLayout from 'components/dashboard/UserLayout';
-import DashboardTitle from 'components/dashboard/Title';
-import EditProfile from 'components/dashboard/EditProfile'
+// Components
+import UserLayout from "components/dashboard/UserLayout";
+import DashboardTitle from "components/dashboard/Title";
+import EditPofileInfo from "components/dashboard/EditInformation";
 const EditInformation = () => {
-    return (
-        <UserLayout>
-            {/* Title  */}
-            <DashboardTitle title="Edit Personal Information" />
-            {/* Title  */}
-            <EditProfile/>
-        </UserLayout>
-    )
-}
+  return (
+    <UserLayout>
+      {/* Title  */}
+      <DashboardTitle title="Edit Personal Information" />
+      {/* Title  */}
+      <EditPofileInfo />
+    </UserLayout>
+  );
+};
 
-export default EditInformation
+export default EditInformation;
