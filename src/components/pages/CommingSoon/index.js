f698948aa124885d674a1e7fdf9b1873a7f1b132
Amjad Saeed <amjadsaeedmail@gmail.com>
2023-05-17 09:27:18 -0700

    comming soon page

--- src/components/pages/CommingSoon/index.js
+++ src/components/pages/CommingSoon/index.js
@@ -13,6 +13,7 @@ const { TextArea } = Input;
 
 const CommingSoon = () => {
   const [data, setData] = useState(null);
+  const [displayPage, setDisplayPage] = useState(true);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -35,76 +36,153 @@ const CommingSoon = () => {
     setData({ days, hours, minutes, seconds });
   };
 
-  return (
-    <Space
-      size={20}
-      direction="vertical"
-      className="w-100 align-items-center mt-5 mb-5"
-    >
-      <Image src={logo} alt="" width={324} height={193} />
-      <Space direction="vertical" className="align-items-center">
-        <CommonHeading heading={"COMING SOON"} />
-        <CommonTextField
-          text={"At What We Do…"}
-          fontSize={"24px"}
-          fontWeight={600}
-        />
-      </Space>
-      <Space>
-        {data?.hours ? (
-          <>
-            <Space direction="vertical">
-              <CommonHeading heading={`${data?.days} - `} />
-              <CommonTextField text={"days"} />
-            </Space>
-            <Space direction="vertical">
-              <CommonHeading heading={`${data?.hours} - `} />
-              <CommonTextField text={"hours"} />
-            </Space>
-            <Space direction="vertical">
-              <CommonHeading heading={`${data?.minutes} - `} />
-              <CommonTextField text={"Minutes"} />
-            </Space>
-            <Space direction="vertical">
-              <CommonHeading heading={`${data?.seconds}`} />
-              <CommonTextField text={"Seconds"} />
-            </Space>
-          </>
-        ) : (
-          <CommonHeading heading={"Loading..."} />
-        )}
-      </Space>
+  const disappearWrapper = () => {
+    setDisplayPage(false);
+  };
 
-      <Space size={20} direction="vertical" className="align-items-center">
-        <CommonTextField
-          text={"Contact Form"}
-          fontWeight={600}
-          color="var(--primary-color)"
-        />
-        <CommonTextField
-          text={"Get In Touch"}
-          fontSize={"35px"}
-          fontWeight={600}
-        />
-        <Row gutter={[14, 14]} className="signUp-main">
-          <Col span={24}>
-            <CommonInputField placeholder="Your Name" />
-          </Col>
-          <Col span={24}>
-            <CommonInputField placeholder="Email" />
-          </Col>
-          <Col span={24}>
-            <CommonInputField placeholder="Contact" type={"number"} />
-          </Col>
-          <Col span={24}>
-            <TextArea placeholder="Message" rows={4} />
-          </Col>
-          <Col span={24}>
-            <CommonButton child="Send" />
-          </Col>
-        </Row>
-      </Space>
-    </Space>
+  return (
+    <div className="coming_soon_bg">
+      <div
+        className="coming_soon_wrapper"
+        onClick={disappearWrapper}
+        style={{ border: `${displayPage ? "2px solid #000000" : ""}` }}
+      >
+        <div className="one_sided_handle_left">
+          <div className="handle"></div>
+          <div className="hook"></div>
+        </div>
+        <div className="one_sided_handle_right">
+          <div className="handle"></div>
+          <div className="hook"></div>
+        </div>
+        <div className="coming_soon_time">
+          <CommonHeading
+            level={3}
+            heading={"iBall Website, iBall App"}
+            color={"#000000"}
+          />
+          <CommonHeading heading={"Coming Soon"} color={"#000000"} />
+          <Space>
+            {data?.hours ? (
+              <>
+                <Space direction="vertical">
+                  <CommonHeading
+                    heading={`${data?.days} - `}
+                    color={"#000000"}
+                  />
+                  <CommonTextField text={"days"} color={"#000000"} />
+                </Space>
+                <Space direction="vertical">
+                  <CommonHeading
+                    heading={`${data?.hours} - `}
+                    color={"#000000"}
+                  />
+                  <CommonTextField text={"hours"} color={"#000000"} />
+                </Space>
+                <Space direction="vertical">
+                  <CommonHeading
+                    heading={`${data?.minutes} - `}
+                    color={"#000000"}
+                  />
+                  <CommonTextField text={"Minutes"} color={"#000000"} />
+                </Space>
+                <Space direction="vertical">
+                  <CommonHeading
+                    heading={`${data?.seconds}`}
+                    color={"#000000"}
+                  />
+                  <CommonTextField text={"Seconds"} color={"#000000"} />
+                </Space>
+              </>
+            ) : (
+              <CommonHeading heading={"Loading..."} />
+            )}
+          </Space>
+        </div>
+        <div className="coming_soon_message">
+          <CommonHeading heading={"STAY TUNED"} color={"#000000"} />
+        </div>
+        <div className="double_sided_handle_left">
+          <div className="hook_top"></div>
+          <div className="handle"></div>
+          <div className="hook_bottom"></div>
+        </div>
+        <div className="double_sided_handle_right">
+          <div className="hook_top"></div>
+          <div className="handle"></div>
+          <div className="hook_bottom"></div>
+        </div>
+      </div>
+    </div>
+    // <Space
+    //   size={20}
+    //   direction="vertical"
+    //   className="w-100 align-items-center mt-5 mb-5"
+    // >
+    //   <Image src={logo} alt="" width={324} height={193} />
+    //   <Space direction="vertical" className="align-items-center">
+    //     <CommonHeading heading={"COMING SOON"} />
+    //     <CommonTextField
+    //       text={"At What We Do…"}
+    //       fontSize={"24px"}
+    //       fontWeight={600}
+    //     />
+    //   </Space>
+    //   <Space>
+    //     {data?.hours ? (
+    //       <>
+    //         <Space direction="vertical">
+    //           <CommonHeading heading={`${data?.days} - `} />
+    //           <CommonTextField text={"days"} />
+    //         </Space>
+    //         <Space direction="vertical">
+    //           <CommonHeading heading={`${data?.hours} - `} />
+    //           <CommonTextField text={"hours"} />
+    //         </Space>
+    //         <Space direction="vertical">
+    //           <CommonHeading heading={`${data?.minutes} - `} />
+    //           <CommonTextField text={"Minutes"} />
+    //         </Space>
+    //         <Space direction="vertical">
+    //           <CommonHeading heading={`${data?.seconds}`} />
+    //           <CommonTextField text={"Seconds"} />
+    //         </Space>
+    //       </>
+    //     ) : (
+    //       <CommonHeading heading={"Loading..."} />
+    //     )}
+    //   </Space>
+
+    //   <Space size={20} direction="vertical" className="align-items-center">
+    //     <CommonTextField
+    //       text={"Contact Form"}
+    //       fontWeight={600}
+    //       color="var(--primary-color)"
+    //     />
+    //     <CommonTextField
+    //       text={"Get In Touch"}
+    //       fontSize={"35px"}
+    //       fontWeight={600}
+    //     />
+    //     <Row gutter={[14, 14]} className="signUp-main">
+    //       <Col span={24}>
+    //         <CommonInputField placeholder="Your Name" />
+    //       </Col>
+    //       <Col span={24}>
+    //         <CommonInputField placeholder="Email" />
+    //       </Col>
+    //       <Col span={24}>
+    //         <CommonInputField placeholder="Contact" type={"number"} />
+    //       </Col>
+    //       <Col span={24}>
+    //         <TextArea placeholder="Message" rows={4} />
+    //       </Col>
+    //       <Col span={24}>
+    //         <CommonButton child="Send" />
+    //       </Col>
+    //     </Row>
+    //   </Space>
+    // </Space>
   );
 };
 
