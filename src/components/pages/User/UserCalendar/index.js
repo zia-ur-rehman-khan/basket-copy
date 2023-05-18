3c5fd0fd6fe7b8a241a5f71c6cfe8daf57d87f91
Ab-Qadir1998 <abdulqadir12356@gmail.com>
2023-05-18 21:00:10 +0500

    fixing  QA report

--- src/components/pages/User/UserCalendar/index.js
+++ src/components/pages/User/UserCalendar/index.js
@@ -41,6 +41,7 @@ const Index = () => {
     new Date(2023, 3, 1),
     new Date(2023, 3, 5),
   ]);
+
   useEffect(() => {
     const calendarDays = document.querySelectorAll(
       ".react-calendar__month-view__weekdays__weekday abbr"
@@ -49,6 +50,9 @@ const Index = () => {
       daysIndex.innerHTML = daysIndex.innerText[0];
     });
   }, []);
+  const locale = {
+    firstDayOfWeek:1,
+  };
 
   // Tabs Content
   const tabItems = [
@@ -94,7 +98,11 @@ const Index = () => {
           xs={{ span: 24 }}
         >
           <div className="calendar__wrapp">
-            <Calendar onChange={setDate} defaultValue={date} startWeekDay={0} />
+            <Calendar
+              onChange={setDate}
+              // defaultValue={date}
+              locale={locale}
+            />
             <ul className="events__highlighter">
               <li className="current__events">
                 <CommonTextField
